6f9c336fe0116ab6aa34d7d36ae5d3210e6b9e9e
Mukesh Swain <161455068+MukeshSwain@users.noreply.github.com>
2025-03-27 09:15:34 +0530

    Update vite.config.js

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -7,6 +7,15 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+  proxy: {
+    '/api': {  // 🔹 All requests starting with "/api" will be proxied
+      target: 'advance-todo-back-qh04aj1n8-mukesh-swains-projects.vercel.app',  // 🔹 Your backend server (Node.js + Express)
+      changeOrigin: true,  // 🔹 Changes the origin of the request to match the target
+      secure: false  // 🔹 Disables SSL verification (useful for HTTP backend)
+    }
+  }
+},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
